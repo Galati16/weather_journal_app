1cc1748632dfae6cdc05e8f155548d933923fd05
Galati16 <galati2@web.de>
2020-04-21 17:00:30 +0200

    functioning version of the task

--- server.js
+++ server.js
@@ -2,25 +2,52 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
+const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 3000;
+const server = app.listen(port, () => { console.log(`running on localhost: ${port}`) })
+
+//app.get('/website', addData);
+
+
 // Callback to debug
 
 // Initialize all route with a callback function
+// GET route
+// GET route
+
+app.get('/data', sendData);
+// POST route
+app.post("/data", saveData);
 
 // Callback function to complete GET '/all'
+function sendData(request, response) {
+    response.send(projectData);
+    console.log(projectData)
+    console.log('get from server');
+};
+// Callback function to complete POST to server '/data'
+function saveData(request, respond) {
+    console.log('post to server')
+    const newData = {
+        temperatur: request.body.temperatur,
+        date: request.body.date,
+        userResponse: request.body.userResponse
+    };
+    projectData = newData;
 
-// Post Route
+}
--- website/app.js
+++ website/app.js
@@ -1,28 +1,95 @@
 /* Global Variables */
 const buttonStart = document.getElementById('generate');
 
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
-console.log(newDate);
+
 // Personal API Key for OpenWeatherMap API
-key = '';
+const baseUrl = 'http://api.openweathermap.org/data/2.5/forecast?';
+const corsvar = 'https://cors-anywhere.herokuapp.com/'
+const key = '&appid=a50025fbaef94083e1bbf95dfe9f0a0b';
+const localurl = 'http://localhost:3000/data'
+
 // Event listener to add function to existing HTML DOM element
-buttonStart.addEventListener('click', function(key) {
-    SendRequest();
-    updateDome();
+buttonStart.addEventListener('click', function() {
+    //assign changed values:
+    const zip = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+
+    //assamble url:
+    const Url = `${corsvar}${baseUrl}zip=${zip},de${key}`;
+    updatePost(Url, feelings);
 });
+
 /* Function called by event listener */
-function SendRequest() {
-    alert('And off we go')
+async function updatePost(url, feelings) {
+    const newTempData = getWebApiData(url)
+        .then(function(temperatur) {
+            uploadData(localurl, temperatur, feelings, newDate);
+            updateDome(localurl);
+        });
 };
+
 /* Function to GET Web API Data*/
+async function getWebApiData(url) {
+    const respond = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': 'http://localhost:3000/',
+        },
+        credentials: 'same-origin',
+    });
+    try {
+        const data = await respond.json();
+        const tempCelsius = data.list[0].main.temp - 273.15;
+        return tempCelsius;
+    } catch (error) {
+        console.log('error is:', error);
+    }
+};
 
 /* Function to POST data */
-
+async function uploadData(localurl, newTempData, feelings, newDate) {
+    console.log('1.PostData');
+    const data = {
+        temperatur: newTempData.toFixed(2),
+        date: newDate,
+        userResponse: feelings
+    };
+    const response = await fetch(localurl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+    try {
+        const newdata = await response.json()
+        return newdata
+    } catch (error) {
+        console.log('error', error);
+    }
+};
 /* Function to update DOM */
-function updateDome() {
-    document.getElementById('date').innerHTML = newDate;
-    document.getElementById('temp').innerHTML = '00°C';
-    document.getElementById('centent').innterHTML = 'bubb';
+async function updateDome(localurl) {
+
+
+    const response = await fetch(localurl, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    try {
+        const dataFromServer = await response.json();
+        document.getElementById('date').innerHTML = dataFromServer.date;
+        document.getElementById('temp').innerHTML = dataFromServer.temperatur + ' °C';
+        document.getElementById('content').innerHTML = dataFromServer.userResponse;
+        return dataFromServer;
+    } catch (error) {
+        console.log('error', error);
+    }
 };
